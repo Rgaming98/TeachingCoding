005c47aed166b87e938cb04e00b86123108bc087
Rgaming98 <59910462+Rgaming98@users.noreply.github.com>
2020-04-24 02:59:12 +0530

    Class 1
    Class One

---
+++ Teaching.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+print("Hello World")
+
+#Printing Hello World (Printing a string)
+
+
+# In[2]:
+
+
+print(1+2)
+
+#Adding of Integers
+
+
+# In[14]:
+
+
+print(2a+2b) 
+
+#Syntax Errors
+
+
+# In[15]:
+
+
+print("Hello"+"World")
+
+#Adding Strings
+
+
+# In[5]:
+
+
+print("Hello"+""+"World")
+
+
+# In[6]:
+
+
+print("Hello"+" "+"World")
+
+#Using a space in string
+
+
+# In[7]:
+
+
+print("Hello"+" "+"World")
+
+
+# In[16]:
+
+
+a = 2
+b = 3
+
+print(A+B)
+
+'''This is a comment'''
+
+
+# In[9]:
+
+
+a = 2
+b = 3
+
+print(a+b+5)
+
+
+# In[10]:
+
+
+a = "The man is mad"
+
+
+# In[11]:
+
+
+print(a)[0]
+
+
+# In[12]:
+
+
+print((a)[0])
+
+
+# In[17]:
+
+
+#Slicing of characters
+
+
+# In[18]:
+
+
+a = "Rutik and Hrithika"
+
+print(a)
+
+
+# In[19]:
+
+
+print(a[3])
+
+
+# In[20]:
+
+
+print(a[5])
+
+
+# In[21]:
+
+
+print(a[-1])
+
+
+# In[22]:
+
+
+print(a[-5])
+
+
+# In[23]:
+
+
+print(a[::-1])
+
+
+# In[24]:
+
+
+print(a[::2])
+
+
+# In[ ]:
+
+
+# a = ("boob")
+b = input("Enter boob")
+
+if a == b:
+    print("The string is a palindrome")
+else:
+    pass
+
+'''This is a condition where in you tell the computer that if so and so value is so, then do this or else do something else
+or pass'''
+
+
+# In[32]:
+
+
+a = 5
+
+b = 5
+
+c = int(input("Enter a number"))
+
+print(a+b+c)
+
+
+# In[33]:
+
+
+print("word"*5)
+
+
+# In[34]:
+
+
+print("word "*5)
+
+
+# In[36]:
+
+
+a = ("boob")
+b = input("Enter boob")
+
+if a == b:
+    print("The string is a palindrome")
+else:
+    print("The string is not a palindrome")
+
+'''This is a condition where in you tell the computer that if so and so value is so, then do this or else do something else
+or pass'''
+
+
+# In[ ]:
+
+
+
+
